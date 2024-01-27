7b1592c0bf8d277c484ce70df7f4656cebb23f2d
MaxBugakov <125705158+MaxBugakov@users.noreply.github.com>
2024-01-27 20:12:16 +0300

    Update BusinessLogic.py

--- BusinessLogic.py
+++ BusinessLogic.py
@@ -19,8 +19,11 @@ def check_network_connection():
 
 
 # Получение данных.
-def fetch_volume(symbol):
-    exchange = ccxt.binance()  # Используйте свою биржу, если не Binance
+def fetch_volume(symbol, exchange, iteration_counter):
+    print(iteration_counter)
+    if (iteration_counter % 20 == 0):
+        main.bot_messege_set("Прошло 20 минут")
+        print("Бот отправил: Прошло 20 минут")
     timeframe = '1m'
     summ = 0
     print("-----Последние 6 свечек-----")
@@ -36,11 +39,10 @@ def fetch_volume(symbol):
         if (i == 0):
             last_time = candles[0][0]
             last_volume = candles[0][5]
-        # if (i == 1):
-        #     bot_message = f"BTC/USDT\nВремя: {datetime.fromtimestamp(candles[0][0] / 1000.0)}\nОбъём: {candles[0][5]}"
         print(f"{datetime.fromtimestamp(candles[0][0] / 1000.0)} {candles[0][1]} {candles[0][2]} {candles[0][3]} {candles[0][4]} {candles[0][5]}")
     average_volume_5 = summ/5.0
     print(f"Средний объём: {average_volume_5}")
+    bot_sent_message_flag = False
     while True:
         time = exchange.milliseconds() - 60000 * 1
         candles = None
@@ -54,14 +56,16 @@ def fetch_volume(symbol):
         # print(f"{last_time} - {datetime.fromtimestamp(last_time / 1000.0)}")
         # print(f"{candles[0][0]} - {datetime.fromtimestamp(candles[0][0] / 1000.0)}")
         if (candles[0][0] == last_time):
-            if (average_volume_5 * 3 <= volume):
+            if (average_volume_5 * 3 <= volume and bot_sent_message_flag == False):
                 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 print(f"BTC/USDT \n Средний объём: {average_volume_5}   Объём на последней свече: {volume} \n Объём вырос в {volume / average_volume_5} раза")
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 bot_message = f"BTC/USDT\nВремя: {datetime.fromtimestamp(candles[0][0] / 1000.0)}\nСредний объём 5: {average_volume_5}\nОбъём: {volume}\nОбъём вырос в {volume / average_volume_5} раза"
-                print("Start")
+                print("Bot is ready")
                 main.bot_messege_set(bot_message)
-                print("Stop")
+                print("Bot has sent message")
                 sleep(2)
+                bot_sent_message_flag = True
             else:
                 sleep(2)
                 continue

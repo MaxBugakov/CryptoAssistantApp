5c102effaec23944a37cafb82458a88465eccd03
Max Bugakov <max.bugakov18@gmail.com>
2024-02-21 01:26:44 +0300

    com

--- Bot.py
+++ Bot.py
@@ -1,12 +1,13 @@
-# Бибилиотека python-telegram-bot 13.0 , так как здесь более удобная работа с ботом, которая не занимает целый поток.
+# Бибилиотека python-telegram-bot 13.15 , так как здесь более удобная работа с ботом, которая не занимает целый поток.
 from telegram import Bot
 
-# Данные бота.
-bot_token = '6500188821:AAET5x2AOab8toa31NiL2V_u3LuOKhkXSBA'
-chat_id = '1180171947'
 
-# Отправка сообщения для бота.
+# Данные бота.
+bot_token = '6500188821:AAFhCttcfBMmEdES3gjlwkWg9JcXrRHewSI'
+chat_id = '-4129403273'
+# chat_id_max = '1180171947'
+
+# Отправка сообщения от бота.
 def send_message(message):
     bot = Bot(token=bot_token)
     bot.send_message(chat_id, text=message)
-
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from App import App
 
+
 # Main.
 if __name__ == "__main__":
     app = App()
-
-
-
-
-
-
